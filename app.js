1d60e6442057701b3c9917f111be474a61f4acc8
rSamokhin <poshliemail@googlemail.com>
2015-10-10 00:58:29 +0300

    file log

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var route = require('koa-route');
 var koa = require('koa');
 var bodyParser = require('koa-body-parser');
 var app = koa();
+var fs = require('fs');
 var sendmail = require('sendmail')();
 
 app.use(serve('web/build/'));
@@ -14,8 +15,9 @@ app.use(route.post('/task', function *(){
     var req = this.request.body,
         email = 'motokeeper@mail.ru',
         from = 'taskmail@motokeeper.ru',
-        task = JSON.stringify(req,null,2)
-
+        task = '';
+    req.date = new Date();
+    task = JSON.stringify(req,null,2);
     sendmail({
         from: from,
         to: email,
@@ -23,6 +25,9 @@ app.use(route.post('/task', function *(){
         content: task
     }, function(err, reply) {
         console.log(err && err.stack);
+        fs.appendFile('./../message.txt', task, function (err) {
+            console.log('err')
+        });
         console.dir(reply);
     });
 
